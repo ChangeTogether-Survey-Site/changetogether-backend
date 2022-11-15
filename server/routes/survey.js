6f77513efc3697dc416ed15a21a62cab6d411f0c
Manvibolreach Ouk <vibolreach.m.ouk@gmail.com>
2022-11-15 01:37:07 -0500

    delete auth

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -8,17 +8,6 @@ let passport = require('passport');
 
 let surveyController = require('../controllers/survey');
 
-// helper function for guard purposes
-function requireAuth(req, res, next)
-{
-    // check if the user is logged in
-    if(!req.isAuthenticated())
-    {
-        return res.redirect('/login');
-    }
-    next();
-}
-
 /* GET Route for the Book List page - READ Operation */
 router.get('/', surveyController.displaySurveyList);
 
