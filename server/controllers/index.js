d574647b5e8518111f59285818541056aa30284b
Manvibolreach Ouk <vibolreach.m.ouk@gmail.com>
2022-11-14 22:17:50 -0500

    test with front end

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -16,15 +16,15 @@ module.exports.displayHomePage = (req, res, next) => {
 }
 
 module.exports.displayAboutPage = (req, res, next) => {
-    res.render('index', { title: 'About', displayName: req.user ? req.user.displayName : ''});
+    res.render('index', { title: 'Login', displayName: req.user ? req.user.displayName : ''});
 }
 
 module.exports.displayProductsPage = (req, res, next) => {
-    res.render('index', { title: 'Products', displayName: req.user ? req.user.displayName : ''});
+    res.render('index', { title: 'Create a Survey', displayName: req.user ? req.user.displayName : ''});
 }
 
 module.exports.displayServicesPage = (req, res, next) => {
-    res.render('index', { title: 'Services', displayName: req.user ? req.user.displayName : ''});
+    res.render('index', { title: 'About us', displayName: req.user ? req.user.displayName : ''});
 }
 
 module.exports.displayContactPage = (req, res, next) => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,22 +10,16 @@ router.get('/', indexController.displayHomePage);
 router.get('/home', indexController.displayHomePage);
 
 /* GET About Us page. */
-router.get('/about', indexController.displayAboutPage);
-
-/* GET Products page. */
-router.get('/products', indexController.displayProductsPage);
-
-/* GET Services page. */
-router.get('/services', indexController.displayServicesPage);
-
-/* GET Contact Us page. */
-router.get('/contact', indexController.displayContactPage);
-
-/* GET Route for displaying the Login page */
 router.get('/login', indexController.displayLoginPage);
 
-/* POST Route for processing the Login page */
-router.post('/login', indexController.processLoginPage);
+/* GET Services page. */
+router.get('/create a survey', indexController.displaycreateCreateaSurveyPage);
+
+/* GET Contact Us page. */
+router.get('/about', indexController.displayAboutPage);
+
+/* GET Route for displaying the Login page */
+router.get('/contact', indexController.displayContactPage);
 
 /* GET Route for displaying the Register page */
 router.get('/register', indexController.displayRegisterPage);
