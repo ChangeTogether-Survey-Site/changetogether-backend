fd8b7b58d1fb92df63c7b6c2743cb52d72ae5b94
Alejandro A <ale@doges.dev>
2022-12-12 00:20:09 -0500

    users.router: created login route. Implemented JWT token creation if credentials are correct

--- server/routes/users.router.js
+++ server/routes/users.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -25,6 +26,28 @@ router.post("/signup", (req, res, next) => {
         });
     });
   });
+});
+
+router.post("/login", (req, res, next) => {
+  // validate if email exists
+  User.find({ email: req.body.email })
+    .then(user=> {
+      if (!user){
+        // if user does not exist
+        return res.status(401).json({"message": "Auth has failed"});
+      } // end if
+      // user exists, validation of pass
+      bcrypt.compare(req.body.password, user.password);
+    })
+      .then(res => {
+        if (!res){
+          return res.status(401).json({"message": "Auth has failed"});
+        }
+        // correct pass comparison (JWT)
+        const token = jwt.sign( { email: user.email, userId: user._id }, 'Chewbacca', { expiresIn: "1h" } );
+      }).catch(err => {
+        return res.status(401).json({"message": "Auth has failed"});
+      })
 });
 
 /* GET users listing. */
