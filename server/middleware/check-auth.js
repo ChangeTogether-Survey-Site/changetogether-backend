a50ff5c8b00b55438ad15bb594803d538fb1efd8
Alejandro A <ale@doges.dev>
2022-12-15 02:57:11 -0500

    backend authentication for EDIT and DELETE survey

--- server/middleware/check-auth.js
+++ server/middleware/check-auth.js
@@ -4,7 +4,8 @@ module.exports = (req, res, next) => {
     // check token
     try {
         const token = req.headers.authorization.split(" ")[1]; // "Bearer token_values"
-        jwt.verify(token, 'Chewbacca');
+        const decodedToken = jwt.verify(token, 'Chewbacca');
+        req.userData = { email: decodedToken.email, userId: decodedToken.userId };
         next();
     } catch (error) {
         res.status(401).json({ message: 'Auth incorrect'});
